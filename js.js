f3840b0e423d060cfb67e7c3b18e870358a4ea28
Mahdiyeh <101658092+mahdiyeh-asgharpour@users.noreply.github.com>
2022-11-15 12:51:07 +0330

    Add files via upload

--- js.js
+++ js.js
@@ -2,17 +2,42 @@ var n = prompt("n:");
 
 for (let i = n; i >= 1; i--) {
     const divs = document.getElementById("a");
-    divs.innerHTML += `<span style='border:10px solid white; background-color:white; color:black;'>${i}</span> <br><br>`;
+    divs.innerHTML += `<span style='border:10px solid white; border-radius:50px; background-color:white; color:black;
+   '>${i}</span> <br><br>`;
 }
 var A, B, C;
 var m = 2 * n;
 
+function AtoC() {
+    const removea = document.getElementById("a");
+    setTimeout(removea.style.opacity = 0, 2000);
+    removea.style.opacity = 0;
+    const addc = document.getElementById("c");
+    addc.innerHTML = removea;
+    setTimeout(addc.style.opacity = 1, 2000);
+
+    removea.remove();
+
+}
+
+function BtoC() {
+    const removea = document.getElementById("b");
+    setTimeout(removea.style.opacity = 0, 2000);
+    removea.style.opacity = 0;
+    const addc = document.getElementById("c");
+    addc.innerHTML = removea;
+    setTimeout(addc.style.opacity = 1, 2000);
+
+    removea.remove();
+
+}
+
 function Hanoi(A, B, C, n) {
     if (n == 1)
-        A-- > C
+        AtoC();
     else {
         Hanoi(A, C, B, n - 1)
-        A-- > C
+        AtoC();
         Hanoi(B, A, C, n - 1)
 
 
@@ -22,13 +47,13 @@ function Hanoi(A, B, C, n) {
 
 function Ex_Hanoi(A, B, C, n) {
     if (n == 1) {
-        A-- > C
-        B-- > C
+        AtoC();
+        BtoC();
     } else {
         Ex_Hanoi(A, B, C, n - 1)
 
         Hanoi(C, A, B, m - 2)
-        A-- > C
+        AtoC();
         Hanoi(B, A, C, m - 1)
 
     }
