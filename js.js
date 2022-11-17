f4be855b9baf2a6506c1bd859be4e12d9cb36920
Mahdiyeh <101658092+mahdiyeh-asgharpour@users.noreply.github.com>
2022-11-17 16:14:03 +0330

    Add files via upload

--- js.js
+++ js.js
@@ -1,60 +1,61 @@
-var n = prompt("n:");
+const n = prompt("n:");
+const A = [];
+const B = [];
+const C = [];
+const m = 2 * n;
+document.body.addEventListener("keypress", Ex_Hanoi);
 
-for (let i = n; i >= 1; i--) {
+for (let i = 0; i < n; i++) {
     const divs = document.getElementById("a");
-    divs.innerHTML += `<span style='border:10px solid white; border-radius:50px; background-color:white; color:black;
-   '>${i}</span> <br><br>`;
+    const a = divs.innerHTML += `<span style='border:10px solid white; border-radius:50px; background-color:white; color:black;
+   '>${n-i}</span> <br><br>`;
+    A[i] = a;
 }
-var A, B, C;
-var m = 2 * n;
 
-function AtoC() {
-    const removea = document.getElementById("a");
-    setTimeout(removea.style.opacity = 0, 2000);
-    removea.style.opacity = 0;
-    const addc = document.getElementById("c");
-    addc.innerHTML = removea;
-    setTimeout(addc.style.opacity = 1, 2000);
+function timer(elementId) {
+    const inp = document.getElementById(elementId);
+    inp.remove();
+    inp.innerHTML = `<input id="Click" type="button" value="Click"> `;
 
-    removea.remove();
 
 }
 
-function BtoC() {
-    const removea = document.getElementById("b");
-    setTimeout(removea.style.opacity = 0, 2000);
-    removea.style.opacity = 0;
-    const addc = document.getElementById("c");
-    addc.innerHTML = removea;
-    setTimeout(addc.style.opacity = 1, 2000);
+function move(index) {
+    if (index == 0) {
+        for (let i = 0; i < n; i++) {
+            C[i] = A[i];
+            A.splice(i, 1);
+        }
 
-    removea.remove();
 
-}
 
-function Hanoi(A, B, C, n) {
-    if (n == 1)
-        AtoC();
-    else {
-        Hanoi(A, C, B, n - 1)
-        AtoC();
-        Hanoi(B, A, C, n - 1)
-
+    } else {
+        for (let i = 0; i < n; i++) {
+            C[i] = B[i];
+            B.splice(i, 1);
+        }
 
     }
 
 }
 
+function Hanoi(A, B, C, n) {
+    if (n == 1) move(0);
+    else {
+        Hanoi(A, C, B, n - 1);
+        move(0);
+        Hanoi(B, A, C, n - 1);
+    }
+}
+
 function Ex_Hanoi(A, B, C, n) {
     if (n == 1) {
-        AtoC();
-        BtoC();
+        move(0);
+        move(1);
     } else {
-        Ex_Hanoi(A, B, C, n - 1)
-
-        Hanoi(C, A, B, m - 2)
-        AtoC();
-        Hanoi(B, A, C, m - 1)
-
+        Ex_Hanoi(A, B, C, n - 1);
+        Hanoi(C, A, B, m - 2);
+        move(0);
+        Hanoi(B, A, C, m - 1);
     }
 }
