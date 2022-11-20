9bec0bd7fbb876387ba922957d35e0958d8d149f
Mahdiyeh <101658092+mahdiyeh-asgharpour@users.noreply.github.com>
2022-11-20 19:52:56 +0330

    Add files via upload

--- p1/Main.java
+++ p1/Main.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        String input = a.next();
+        int n = input.length();
+        string permvtation=new string();
+        string.permvtation(input,0,n-1);
+
 
     }
 
-    }
+}
--- p1/string.java
+++ p1/string.java
@@ -4,22 +4,37 @@ import com.sun.org.apache.bcel.internal.generic.SWAP;
 
 import java.util.Scanner;
 
+import static java.util.Collections.swap;
+
 public class string {
-    Scanner a=new Scanner(System.in);
-    String s=a.next();
-    int n=s.length();
     int m=1;
-    public void  permvtation( String s ,int m,int n){
+    public static void  permvtation(String s, int m, int n){
         if (m==n){
             System.out.println(s+"\n");
         }else {
             for (int i=m;i<=n;i++)
             {
-                SWAP(s[m],s[i]);
+                s= swap(s,m,i);
                 permvtation(s,m+1,n);
-                SWAP(s[m],s[i]);
+                s= swap(s,m,i);
+
             }
+            System.out.println(s+"\n");
         }
     }
+    public static String swap(String s, int m, int n)
+    {
+        char temp;
+        char[] chars=s.toCharArray();
+
+        temp =chars[m];
+        chars[m]=chars[n];
+        chars[n]=temp;
+        return  String.valueOf(chars);
+
+    }
+
+
+
 
 }
