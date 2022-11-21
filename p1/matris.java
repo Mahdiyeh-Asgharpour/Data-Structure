ba985d8fa0931de8ca91d90bdfc9af9c36733d42
Mahdiyeh <101658092+mahdiyeh-asgharpour@users.noreply.github.com>
2022-11-21 16:51:08 +0330

    Add files via upload

--- p1/matris.java
+++ p1/matris.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 import static java.lang.Math.pow;
 
 public class matris {
+
     int n;
     int[][] matrix = new int[n][n];
 
-     static void inter(int matrix[][], int n) {
+    static void inter(int matrix[][], int n) {
         Scanner b = new Scanner(System.in);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -18,52 +19,45 @@ public class matris {
         }
     }
 
-    static void b(int matrix2[][], int matrix[][], int i, int n) {
-        int q = 0, w = 0;
-        for (int j = 1; j < n; j++) {
-            for (int k = 1; k < n; k++) {
-                if (k == i) {
-                    continue;
-                }
-                matrix2[q][w] = matrix[j][k];
-                w++;
-                if (w == n - 1) {
-                    q++;
-                    w = 0;
-                }
-            }
-        }
-    }
-
-     static  int det(int matrix[][], int n) {
-        int temp = 0;
+    public static int det(int matrix[][], int n) {
         int sum = 0;
-        int[][] matrix2 = new int[n][n];
-        if (n == 2) {
+        int matrix2[][] = new int[n][n];
+        if (n == 1) return matrix[0][0];
+        else if (n == 2) {
             sum = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
-            return  sum;
+            return sum;
         } else {
-            for (int i = 0; i < n; i++) {
-                b(matrix2, matrix, i, n);
-                sum += matrix[0][1] * pow(-1,i) * det(matrix2, n - 1);
+            for (int k = 0; k < n; k++) {
+                int q = 0;
+                int w = 0;
+
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        if (j == k) continue;
+                        matrix2[q][w] = matrix[i][j];
+                        w++;
+                        if (w == n - 1) {
+                            q++;
+                            w = 0;
+                        }
+                    }
+                }
+                sum += sum+ matrix[0][k] * pow(-1, k) * det(matrix2, n - 1);
 
             }
+
         }
         return sum;
-
-
     }
-
 
     public static void main(String[] args) {
         System.out.println("n:");
-        Scanner a=new Scanner(System.in);
-        int n =a.nextInt();
+        Scanner a = new Scanner(System.in);
+        int n = a.nextInt();
         int[][] matrix = new int[n][n];
-        inter(matrix,n);
-        int det=det(matrix,n);
+        inter(matrix, n);
+        int det = det(matrix, n);
         System.out.println(det);
-
 
 
     }
